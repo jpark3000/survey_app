34348d302a248222ebb2b3fda15528db954a3c93
Apprentice <jpark3000@gmail.com>
2014-02-07 16:31:55 -0600

    added taking survey view

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -21,10 +21,24 @@ post '/create_survey' do
     question = Question.create(content: params[(num + 1).to_s]["question"], key:(num +1))
     survey.questions << question
     (params[(num + 1).to_s].keys.length - 2).times do |key_num|
-      question.choices << Choice.create(content: params[LETTERS[key_num+1]], key:LETTERS[key_num+1])
+      question.choices << Choice.create(content: params[(num + 1).to_s][LETTERS[key_num+1]], key:LETTERS[key_num+1])
     end
   end
 
   survey.id.to_s
 end
 
+get '/survey/:survey_id' do
+  @survey = Survey.find(params[:survey_id])
+
+  erb :survey
+end
+
+post '/survey' do
+  "asdfsdafsad"
+
+  # user = User.find(session[:user_id])
+  # user.choices << Choice.find(params[:choice_id])
+
+
+end
